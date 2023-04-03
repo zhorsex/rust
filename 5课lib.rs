88ddb908f2f1c9b0e96dec1f2957ec136804ddc3
zhorsex <41467951+zhorsex@users.noreply.github.com>
2023-04-03 17:24:36 +0800

    Create 5课lib.rs
    5课

---
+++ 5课lib.rs
@@ -0,0 +1,184 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/v3/runtime/frame>
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use sp_std::vec::Vec;
+
+	// Runtime Configuration Trait
+	// All types and constants go here.
+	// Use #[pallet::constant] and #[pallet::extra_constants]
+	// to pass in values to metadata.
+	// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// The minimum length a name may be.
+		#[pallet::constant]
+		type MinLength: Get<u32>;
+
+		/// The maximum length a name may be.
+		#[pallet::constant]
+		type MaxLength: Get<u32>;
+	}
+
+	// Declaration of the Pallet type
+	// This is a placeholder to implement traits and methods.
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// Runtime Storage
+	// Use to declare storage items.
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/v3/runtime/storage
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
+	#[pallet::storage]
+	#[pallet::getter(fn proofs)]
+	pub(super) type Proofs<T: Config> =
+		StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber)>;
+
+	// Hooks
+	// Define some logic that should be executed
+	// regularly in some context, for e.g. on_initialize.
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	// Runtime Events
+	// Can stringify event types to metadata.
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// Event emitted when a proof has been claimed. [who, claim]
+		ClaimCreated(T::AccountId, Vec<u8>),
+		/// Event emitted when a claim is revoked by the owner. [who, claim]
+		ClaimRevoked(T::AccountId, Vec<u8>),
+		/// Event emitted when a claim is transferred by the owner. [from_who, to_who, claim]
+		ClaimTransferred(T::AccountId, T::AccountId, Vec<u8>),
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// The proof has already been claimed.
+		ProofAlreadyClaimed,
+		/// The proof does not exist, so it cannot be revoked.
+		NoSuchProof,
+		/// The proof is claimed by another account, so caller can't revoke it.
+		NotProofOwner,
+		/// The proof is too short.
+		ProofTooShort,
+		/// The proof is too long.
+		ProofTooLong,
+	}
+
+	// Extrinsics
+	// Functions that are callable from outside the runtime.
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(1_000)]
+		pub fn create_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
+
+			ensure!(proof.len() >= T::MinLength::get() as usize, Error::<T>::ProofTooShort);
+			ensure!(proof.len() <= T::MaxLength::get() as usize, Error::<T>::ProofTooLong);
+
+			// Verify that the specified proof has not already been claimed.
+			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+
+			// Get the block number from the FRAME System pallet.
+			let current_block = <frame_system::Pallet<T>>::block_number();
+
+			// Store the proof with the sender and block number.
+			Proofs::<T>::insert(&proof, (&sender, current_block));
+
+			// Emit an event that the claim was created.
+			Self::deposit_event(Event::ClaimCreated(sender, proof));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn revoke_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
+
+			ensure!(proof.len() >= T::MinLength::get() as usize, Error::<T>::ProofTooShort);
+			ensure!(proof.len() <= T::MaxLength::get() as usize, Error::<T>::ProofTooLong);
+
+			// Verify that the specified proof has been claimed.
+			// Get owner of the claim.
+			let (owner, _) = Proofs::<T>::get(&proof).ok_or(Error::<T>::NoSuchProof)?;
+
+			// Verify that sender of the current call is the claim owner.
+			ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+			// Remove claim from storage.
+			Proofs::<T>::remove(&proof);
+
+			// Emit an event that the claim was erased.
+			Self::deposit_event(Event::ClaimRevoked(sender, proof));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn transfer_claim(
+			origin: OriginFor<T>,
+			to: T::AccountId,
+			proof: Vec<u8>,
+		) -> DispatchResultWithPostInfo {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
+
+			ensure!(proof.len() >= T::MinLength::get() as usize, Error::<T>::ProofTooShort);
+			ensure!(proof.len() <= T::MaxLength::get() as usize, Error::<T>::ProofTooLong);
+
+			// Verify that the specified proof has been claimed.
+			// Get owner of the claim.
+			let (owner, block_number) = Proofs::<T>::get(&proof).ok_or(Error::<T>::NoSuchProof)?;
+
+			// Verify that sender of the current call is the claim owner.
+			ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+			// Remove claim from storage.
+			Proofs::<T>::try_mutate(&proof, |v| -> DispatchResultWithPostInfo {
+				*v = Some((to.clone(), block_number));
+				Ok(().into())
+			})?;
+
+			// Emit an event that the claim was transferred.
+			Self::deposit_event(Event::ClaimTransferred(sender, to, proof));
+
+			Ok(().into())
+		}
+	}
+}
